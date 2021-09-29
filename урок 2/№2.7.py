b3d6b0079decd7556ded91da6f7d8529d3f8b82c
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2021-09-29 16:37:01 +0300

    Update №2.7.py

--- урок 2/№2.7.py
+++ урок 2/№2.7.py
@@ -3,8 +3,8 @@
 print('Чтобы узнать какой год високостный')
 def vesokost_god():
 	n= int(input('Введите год '))
-	if n%4==0 or n%400==0:
+	if n%4==0 and n%100!=0 or n%400==0:
 		return('Да')
-	if  n%100!=0:
+	else:
 		return("Нет")
 print(vesokost_god())
