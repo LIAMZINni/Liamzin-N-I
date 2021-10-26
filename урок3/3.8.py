91f89f365f0a9273202792f9f1d3cff153ac07cf
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2021-10-26 19:04:02 +0300

    Create 3.8.py

---
+++ урок3/3.8.py
@@ -0,0 +1,9 @@
+def nam8():
+    n = int(input("Введите число от 1 до 9 - "))
+    if 1 <= n <= 9:
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                print(j, sep='', end='')
+            print()
+
+print(nam8())
