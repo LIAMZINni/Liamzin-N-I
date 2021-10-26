a4ca5e1d1214f4c1870a51d1d85cb23df1e30378
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2021-10-26 20:13:45 +0300

    Create 4.1.py

---
+++ урок 4/4.1.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+def nam1():
+	s=str(input('Введите строку '))
+	print (s[2])
+	print (s[-2])
+	print (s[0:5])
+	print(s[:-2])
+	print(s[::2])
+	print(s[1::2])
+	print(s[::-1])
+	print(s[::-2])
+	print(len(s))
+print(nam1())
