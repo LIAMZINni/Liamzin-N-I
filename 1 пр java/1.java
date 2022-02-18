2d5f4cc2c23868ab6270c7381f3bdaa0f0699822
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2022-02-18 22:45:50 +0300

    Create 1.java

---
+++ 1 пр java/1.java
@@ -0,0 +1,12 @@
+public class java {
+    public static void main(String[] args) {
+
+        if (args.length==0){
+            System.out.println("Hello world ");
+        }
+        else{
+            System.out.println("Hello " + args[0]);
+        }
+
+    }
+}
