b4993b5673c92ddb29fde6df8f6fa69757a37dff
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2022-02-18 23:35:17 +0300

    Create 3.java

---
+++ 1 пр java/3.java
@@ -0,0 +1,13 @@
+public class java {
+    public static void main(String[] args) {
+        int n = args.length;
+        if (n != 2) {
+            System.out.println("Вы не передавали параметров ");
+        } else {
+            System.out.println(args[0] +"+"+args[1]+"="+((Integer.parseInt(args[0])+(Integer.parseInt(args[1])))));
+        }
+    }
+}
+/*Передавать в качестве параметров два целочисленных числа. Вывести на
+экран как сами значения? так и их сумму (“3 + 2 = 5”). Если количество параметров
+не равно 2, вывести сообщение “Неверное количество параметров”.*/
