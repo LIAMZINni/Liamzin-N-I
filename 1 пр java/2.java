993d451b940f76cb95755ca9e1d552e72d2edee9
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2022-02-18 23:08:05 +0300

    Create 2.java

---
+++ 1 пр java/2.java
@@ -0,0 +1,12 @@
+public class java {
+    public static void main(String[] args) {
+        int n= args.length;
+        if (n==0){
+            System.out.println("Вы не передавали параметров ");
+        }
+        else{
+            System.out.println("Вы ввели "  + n+ " параметров" );
+        }
+
+    }
+}
