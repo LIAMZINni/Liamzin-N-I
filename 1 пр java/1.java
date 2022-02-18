6cdd3aaa2a85ade3a130886246403c9d4a352832
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2022-02-18 22:51:08 +0300

    Update 1.java

--- 1 пр java/1.java
+++ 1 пр java/1.java
@@ -1,11 +1,11 @@
 public class java {
     public static void main(String[] args) {
-
-        if (args.length==0){
+        String n=args[0];
+        if (n == ""){
             System.out.println("Hello world ");
         }
         else{
-            System.out.println("Hello " + args[0]);
+            System.out.println("Hello " + n);
         }
 
     }
