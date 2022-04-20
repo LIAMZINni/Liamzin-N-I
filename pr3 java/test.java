1f1e8f5429c1aa3e9d604e9aeff77d421b543826
LIAMZINni <90478644+LIAMZINni@users.noreply.github.com>
2022-04-20 18:49:40 +0300

    Add files via upload

---
+++ pr3 java/test.java
@@ -0,0 +1,10 @@
+package hotel;
+
+public class test {
+    public static void main(String[] args){
+        Room Room=new Room(1,12,2,"Нормальная",1000);
+        System.out.println(Room.tostring());
+        System.out.println(Room);
+
+    }
+}
